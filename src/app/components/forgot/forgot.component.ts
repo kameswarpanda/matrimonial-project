997b4b7333ee580e3a779b86a3ea4a1117b438c4
Kameswar Panda <kameswarpanda11@gmail.com>
2024-05-01 02:57:32 +0530

    signup form validation system

--- src/app/components/forgot/forgot.component.ts
+++ src/app/components/forgot/forgot.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,13 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive, ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
+userForm: FormGroup;
+isFormSubmitted: boolean = false;
+
+constructor(){
+  this.userForm = new FormGroup({
+    name: new FormControl("",[Validators.required, Validators.minLength(2)]),
+    address: new FormControl("",[Validators.required]),
+    mailId: new FormControl("",[Validators.required, Validators.email]),
+    mobileNumber: new FormControl("",[Validators.required, Validators.pattern("[0-9 ]{10}")]),
+    guestNo: new FormControl(""),
+    date: new FormControl(""),
+    password: new FormControl("",[Validators.required, Validators.minLength(8)]),
+
+  })
+}
+
+  onSubmit(){
+    const isFormValid = this.userForm.valid;
+    debugger;
+    this.isFormSubmitted = true;
+  }
 }
